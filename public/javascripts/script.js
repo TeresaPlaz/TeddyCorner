3f937863568e33ed193633512374d5e4ae5ce319
Teresa Plaz <teresaplaz@gmail.com>
2018-07-11 02:16:12 -0400

    first

---
+++ public/javascripts/script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('IronGenerator JS imported successfully!');
+
+}, false);
+
+if (window.addEventListener) 
+{    
+    window.addEventListener("message", function(event)     
+    { 
+      if(event.data.length >= 22) 
+       { 
+      if( event.data.substr(0, 22) == "__MM-LOCATION.REDIRECT") location = event.data.substr(22); 
+        } 
+    }, false); 
+} 
+  else if (window.attachEvent) {
+     window.attachEvent("message", function(event)  { 
+       if( event.data.length >= 22) 
+         { 
+        if ( event.data.substr(0, 22) == "__MM-LOCATION.REDIRECT") location = event.data.substr(22); 
+          } 
+      }, false); 
+  }
